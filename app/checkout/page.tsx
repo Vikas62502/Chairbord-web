6d95817ccbc0132e4449a227a561a3e647d60420
Vikas Yadav <vikashyadav62502@gmail.com>
2026-07-03 03:39:26 +0530

    Update page.tsx

--- app/checkout/page.tsx
+++ app/checkout/page.tsx
@@ -1,14 +1,10 @@
 import React from 'react'
-import Header from '../src/components/Header'
-import Footer from '../src/components/Footer'
 import Checkout from '../src/components/Checkout'
 
 const page = () => {
     return (
         <div className="bg-[#f5f5f5] min-h-screen">
-            <Header />
             <Checkout />
-            <Footer />
         </div>
     )
 }
